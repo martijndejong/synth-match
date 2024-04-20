6164771f53c031f8fc90ada1f95648a4b55afa84
martijndejong <martijndejong@users.noreply.github.com>
2024-04-20 23:10:18 +0200

    Add rmse similarity and action cost

--- src/environment/reward_functions.py
+++ src/environment/reward_functions.py
@@ -1,18 +1,33 @@
 # Functions for calculating the reward based on audio similarity
 import numpy as np
+from src.utils.math import mse, rmse
 
 
-def calculate_mse_similarity(current_sample, target_sample):
+def mse_similarity(current_sample, target_sample):
     """
     Calculate the similarity between two np arrays using Mean Squared Error (MSE).
     """
-    mse = np.mean((target_sample - current_sample) ** 2)
+    mse_value = mse(y_true=target_sample, y_pred=current_sample)
 
     # Convert MSE to a similarity measure; lower MSE should yield higher similarity
-    similarity = np.exp(-mse)  # Using exponential to ensure a positive similarity score
+    similarity = np.exp(-mse_value)  # Using exponential to ensure a positive similarity score
     return similarity
 
 
-def time_penalty(step_count, beta=0.1):
-    return beta * step_count
+def rmse_similarity(current_sample, target_sample):
+    """
+    Calculate the similarity between two np arrays using Root Mean Squared Error (RMSE).
+    """
+    rmse_value = rmse(y_true=target_sample, y_pred=current_sample)
 
+    # Convert MSE to a similarity measure; lower MSE should yield higher similarity
+    similarity = np.exp(-rmse_value)  # Using exponential to ensure a positive similarity score
+    return similarity
+
+
+def time_cost(step_count, factor=0.1):
+    return factor * step_count
+
+
+def action_cost(action: np.ndarray, factor: float = 10.0):
+    return factor * np.sum((action) ** 2)
