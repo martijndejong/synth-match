5029f275b5cb04734c018ebdfffae6fa1843c6e6
martijndejong <mjl.dejong@outlook.com>
2024-05-03 05:30:15 +0200

    Refactor audio processor to be consistent and add cropping/padding functionality

--- src/utils/audio_processor.py
+++ src/utils/audio_processor.py
@@ -11,8 +11,30 @@ class AudioProcessor:
         self.audio_sample = audio_sample
         self.spectrogram = None
 
+    def update_sample(self, audio_sample):
+        self.audio_sample = audio_sample
+        self.convert_to_mono()
+        self.crop_sample(length=2.0)
+        self.calculate_spectrogram()
+
     def convert_to_mono(self):
-        pass
+        if self.audio_sample.shape[0] == 2:
+            self.audio_sample = self.audio_sample.mean(axis=0)
+            return
+        return
+    
+    def crop_sample(self, length: float):
+        """
+        Crop audio sample to specified length in seconds. Add 0 padding if new width longer than original sample.
+        """
+        new_len = int(length * self.sampling_freq)
+        cur_len = len(self.audio_sample)
+        if new_len > cur_len:
+            self.audio_sample = np.pad(self.audio_sample, (0, new_len - cur_len))
+
+        elif new_len <= cur_len:
+            self.audio_sample = self.audio_sample[0:new_len]
+        return
 
     def normalize(self):
         # Normalize self.audio_sample
@@ -28,8 +50,7 @@ class AudioProcessor:
         )
         S_db_mel = librosa.amplitude_to_db(S_mel, ref=np.max)
 
-        self.spectrogram = S_db_mel
+        # FIXME: MAKE NORMALIZATION MORE CONTROLLED INSTEAD OF HARDCODED IN RETURN HERE
+        self.spectrogram = (S_db_mel + 80)/80
 
-        # FIXME: RETURN SPECTOGRAM? OR KEEP IT AS CLASS ATTRIBUTE THAT IS FILLED?
-        #   PLUS MAKE NORMALIZATION MORE CONTROLLED INSTEAD OF HARDCODED IN RETURN HERE
-        return (S_db_mel + 80)/80
+        return
