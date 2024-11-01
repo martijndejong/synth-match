e4cb907975c3eabee83ef74df72e7b6398581bbb
martijndejong <martijndejong@users.noreply.github.com>
2024-11-01 20:58:30 +0100

    Force output to higher dimensionality to fit output shape of spectrogram observer

--- src/observers/parameter_observer.py
+++ src/observers/parameter_observer.py
@@ -4,14 +4,19 @@ This observer directly passes the synthesizer parameters error to the agent netw
 The purpose of this observer is to debug the agent logic in a simplified setup (instead of end-to-end training).
 """
 
-from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
 
 
 def build_parameter_observer(input_shape=(2,)):
     inputs = Input(shape=input_shape)
-    # Directly use the inputs as outputs
-    model = Model(inputs=inputs, outputs=inputs)
+
+    # # Directly use the inputs as outputs
+    # model = Model(inputs=inputs, outputs=inputs)
+
+    # Bring output to higher dimensionality to simulate feature dimensionality of actual observer network
+    outputs = Dense(128, activation='relu')(inputs)
+    model = Model(inputs=inputs, outputs=outputs)
     return model
 
 
