09d7f5f4bc28c642399267f333c31fc6f8f85a1e
Job van Zijl <jobvanzijl@rewirenow.com>
2025-01-03 14:08:02 +0100

    add vue icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
-// removed the following due to Primevue update not having resources anymore
-// import 'primevue/resources/themes/saga-blue/theme.css'; // Choose your theme
-// import 'primevue/resources/primevue.min.css';
-// import 'primeicons/primeicons.css';
-// import './assets/main.css';
+import 'primeicons/primeicons.css';
 
 const app = createApp(App);
 
